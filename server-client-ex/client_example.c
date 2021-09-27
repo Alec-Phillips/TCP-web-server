8b07c229ee44b93814720e5ccaa901a7ef57cc7a
Christopher Linscott <lchris@Christophers-MacBook-Pro.local>
2021-09-26 23:32:48 -0700

    Got filesystem requests sort of working, multiple requestsdon't work at the moment. As well as, we can see requests from browser now (which is really good).

--- server-client-ex/client_example.c
+++ server-client-ex/client_example.c
@@ -11,7 +11,8 @@ int main(int argc , char *argv[])
 {
 	int sock;
 	struct sockaddr_in server;
-	char message[1000] , server_reply[2000];
+	int port = 20;
+	char message[1000], server_reply[2000];
 	
 	//Create socket
 	sock = socket(AF_INET , SOCK_STREAM , 0);
@@ -23,7 +24,7 @@ int main(int argc , char *argv[])
 	
 	server.sin_addr.s_addr = inet_addr("127.0.0.1");
 	server.sin_family = AF_INET;
-	server.sin_port = htons( 8888 );
+	server.sin_port = htons(port);
 
 	//Connect to remote server
 	if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
@@ -35,20 +36,30 @@ int main(int argc , char *argv[])
 	puts("Connected\n");
 	
 	//keep communicating with server
+	char cmdPath[4092];
+	getcwd(cmdPath, 3000);
+	printf("%s\n", cmdPath);
+
+	char* request;
+
 	while(1)
 	{
-		printf("Enter message : ");
-		scanf("%s" , message);
+		puts("What file would you like to access?\n");
+		// printf("The current directory you are looking in:\n %s", cmdPath);
+		scanf("%s", message);
+		request = strcat(cmdPath, message);
+		request = strcat(request, "\n");
 		
+		puts(request);
 		//Send some data
-		if( send(sock , message , strlen(message) , 0) < 0)
+		if(send(sock, request , strlen(request) , 0) < 0)
 		{
 			puts("Send failed");
 			return 1;
 		}
 		
 		//Receive a reply from the server
-		if( recv(sock , server_reply , 2000 , 0) < 0)
+		if(recv(sock, server_reply , 2000 , 0) < 0)
 		{
 			puts("recv failed");
 			break;
@@ -56,6 +67,7 @@ int main(int argc , char *argv[])
 		
 		puts("Server reply :");
 		puts(server_reply);
+		fflush(stdout);
 	}
 	
 	close(sock);
--- server-client-ex/server_example.c
+++ server-client-ex/server_example.c
@@ -2,33 +2,41 @@
 	C socket server example
 */
 
-#include<stdio.h>
-#include<string.h>	//strlen
-#include<sys/socket.h>
-#include<arpa/inet.h>	//inet_addr
-#include<unistd.h>	//write
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>	//strlen
+#include <limits.h> 
+#include <sys/socket.h>
+#include <arpa/inet.h>	//inet_addr
+#include <unistd.h>	//write
+
+
+
 
 int main(int argc , char *argv[])
 {
-	int socket_desc , client_sock , c , read_size;
+	int server_socket , client_sock , c , read_size;
 	struct sockaddr_in server , client;
-	char client_message[2000];
+	char client_message[3000];
+	int server_backlog = 10;
+	int server_port = 20;
 	
 	//Create socket
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-	if (socket_desc == -1)
+	server_socket = socket(AF_INET , SOCK_STREAM , 0);
+	if (server_socket == -1)
 	{
 		printf("Could not create socket");
+		exit(1);
 	}
 	puts("Socket created");
 	
 	//Prepare the sockaddr_in structure
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons( 8888 );
+	server.sin_port = htons( server_port );
 	
 	//Bind
-	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+	if( bind(server_socket, (struct sockaddr *)&server , sizeof(server)) < 0)
 	{
 		//print the error message
 		perror("bind failed. Error");
@@ -37,40 +45,69 @@ int main(int argc , char *argv[])
 	puts("bind done");
 	
 	//Listen
-	listen(socket_desc , 3);
+	listen(server_socket , server_backlog);
+
+	printf("Listening on port: %d\n", server_port);
+
+	char* hello = "HTTP/1.1 200 OK\nDate: Mon, 27 Jul 2009 12:28:53 GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\nContent-Length:88\nContent-Type: text/html\nConnection: Closed\n<html><body><h1>Hello, World!</h1></body><html>";
+	// write(client_sock , hello , strlen(hello));
+
+	char * message = "GET / HTTP/1.1\r\n\r\n";
 	
 	//Accept and incoming connection
 	puts("Waiting for incoming connections...");
 	c = sizeof(struct sockaddr_in);
-	
 	//accept connection from an incoming client
-	client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+	client_sock = accept(server_socket, (struct sockaddr *)&client, (socklen_t*)&c);
 	if (client_sock < 0)
 	{
 		perror("accept failed");
-		return 1;
+		exit(1);
 	}
-	puts("Connection accepted");
-	
-	// char* hello = "HTTP/1.1 200 OK\nDate: Mon, 27 Jul 2009 12:28:53 GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\nContent-Length:88\nContent-Type: text/html\nConnection: Closed\n<html><body><h1>Hello, World!</h1></body><html>";
-	// write(client_sock , hello , strlen(hello));
+	printf("Connection accepted, connection number: %d\n", client_sock);
+	char buffer[4096];
+	char actualpath[PATH_MAX+1];
+	int msgsize = 0;
+	size_t bytesread;
+	//Receive a message (filepath) from client
+	while((bytesread = read(client_sock, buffer+msgsize, sizeof(buffer)-msgsize-1)) > 0)
+	{	
+		msgsize += bytesread;
+		if(msgsize > 4095) break;
+		buffer[msgsize-1] = 0;
+		// Send the message back to client
+		printf("REQUEST IS: %s\n", buffer);
+		printf("REALPATH IS: %s\n", realpath(buffer, actualpath));
+		// if(realpath(buffer, actualpath) == NULL) {
+		// 	printf("ERROR: %s is an incorrect path.\n", buffer);
+		// 	close(client_sock);
+		// 	return 0;
+		// }
+		FILE *fp = fopen(actualpath, "r");
+		if(fp == NULL) {
+			printf("Error in opening file.");
+			close(client_sock);
+			return 0;
+		}
 
-	//Receive a message from client
-	while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
-	{
-		//Send the message back to client
-		write(client_sock , client_message , strlen(client_message));
+		while((bytesread = fread(buffer, 1, 4096, fp)) > 0) {
+			write(client_sock, buffer, bytesread);
+		}
+		// write(client_sock , hello , strlen(hello));
+		// send(server_socket, message, strlen(message), 0);
+		close(client_sock);
+		fclose(fp);
 	}
-	
-	if(read_size == 0)
-	{
-		puts("Client disconnected");
-		fflush(stdout);
-	}
-	else if(read_size == -1)
-	{
-		perror("recv failed");
-	}
+
+	// if(read_size == 0)
+	// {
+	// 	puts("Client disconnected");
+	// 	fflush(stdout);
+	// }
+	// else if(read_size == -1)
+	// {
+	// 	perror("recv failed");
+	// }
 	
 	return 0;
 }
