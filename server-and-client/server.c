1aed9ee53c1ddf1d958fa942ec56789c755e0708
Christopher Linscott <lchris@Christophers-MacBook-Pro.local>
2021-10-25 14:24:20 -0700

    Rewrote to work with the local filesystem inside of our computers, since tthe virtual file system was getting too buggy.

--- server-and-client/server.c
+++ server-and-client/server.c
@@ -6,9 +6,10 @@
 #include <arpa/inet.h>
 #include <time.h>
 #include <unistd.h>
-#include "/Users/alecphillips/Documents/Fall 2021/COMP379/TCP-web-server/file_system/FileSystem.h" // Change
-#include "/Users/alecphillips/Documents/Fall 2021/COMP379/TCP-web-server/file_system/UsefulStructures.h" // Change
+#include "/Users/lchris/Desktop/Coding/schoolprojects/TCP-web-server/file_system/FileSystem.h" // Change
+#include "/Users/lchris/Desktop/Coding/schoolprojects/TCP-web-server/file_system/UsefulStructures.h" // Change
 #include <assert.h>
+#include <pthread.h>
 
 /* 
 	Note:
@@ -25,6 +26,10 @@
 	C socket server example
 	https://www.binarytides.com/server-client-example-c-sockets-linux/
 */
+
+void connection_handler(void* socket_desc);
+void getRealPath(char buffer[], char actualpath[]);
+void getFileContents(char actualpath[], char fileContents[]);
 
 /*
 	HTTP Response Creator
@@ -69,41 +74,15 @@ char* replaceWord(const char* s, const char* oldW,
     return result;
 }
 
-// implement here before I implement POST
-// int testUploadFile(Directory *root) {
-//     char path[] = "/desktop";
-//     char *fileName = "test_file.txt";
-//     char *fileData = "This is a test file";
-//     uploadFile(path, root, fileData, fileName);
-
-//     char filePath[] = "/desktop/test_file.txt";
-//     char *fileContents = openFile(filePath, root);
-
-//     assert(!strcmp(fileContents, "This is a test file"));
-
-//     char newPath[] = "/documents/files";
-//     uploadFile(newPath, root, fileData, fileName);
-
-//     char newFilePath[] = "/documents/files/test_file.txt";
-//     fileContents = openFile(newFilePath, root);
-//     printf("%s\n", fileContents);
-//     assert(!strcmp(fileContents, "This is a test file"));
-
-//     return 0;
-// }
 int testUploadToRoot(Directory *root) {
-
     char path[] = "/desktop";
 	makeDirectory("desktop", root);
     char *fileName = "test_file.txt";
     char *fileData = "This is a test file";
-	
     uploadFile(path, root, fileData, fileName);
     char filePath[] = "/desktop/test_file.txt";
     char *fileContents = openFile(filePath, root);
-
     assert(!strcmp(fileContents, "This is a test file"));
-
     return 0;
 }
 
@@ -146,9 +125,8 @@ int main(int argc, char *argv[])
 	server_backlog: number of clients possible
 	server_port: where server will listen
 	*/
-	int server_socket, client_sock, size_of_addr;
+	int server_socket, client_sock, size_of_addr, *new_sock;
 	struct sockaddr_in server, client;
-	char client_message[3000];
 	int server_backlog = 10;
 	int server_port = atoi(argv[1]);
 	Directory* root = initializeRoot();
@@ -179,8 +157,6 @@ int main(int argc, char *argv[])
 
 	printf("Listening on port: %d\n", server_port);
 
-	char HTTPResponse[4092];
-	
 	char* error_message = "Couldn't get request type.";
 	
 	//Accept incoming connections
@@ -195,122 +171,19 @@ int main(int argc, char *argv[])
 			exit(1);
 		}
 		printf("Connection accepted, connection number: %d\n", client_sock);
+		
+		pthread_t sniffer_thread;
+		new_sock = malloc(1); 
+		*new_sock = client_sock;
 
-		char buffer[4096];
-		int msgsize = 0;
-		size_t bytesread;
-
-		// Begin receiving HTTP requests (either from the browser, Postman, or client)
-		while((bytesread = read(client_sock, buffer+msgsize, sizeof(buffer)-msgsize-1)) > 0)
-		{	
-			msgsize += bytesread;
-			if(msgsize > 4095) break;
-			buffer[msgsize-1] = 0;
-			// Send the message back to client
-			if(msgsize == bytesread) printf("REQUEST IS:\n%s\n", buffer);
-
-			//HTTP Parsing
-			char* requestType = strtok(buffer, " "); // GET, POST, DELETE
-			char* path;
-
-			if(requestType != NULL) {
-				if(root->childDirs != NULL){
-					printf("WE IN HERE");
-					printf("%s\n", root->childDirs->data->name);
-					// if(root->childDirs->data->name == "desktop") puts("THERE IS A DESKTOP HOMIE");
-					puts(root->childDirs->data->files->data);
-				}
-				path = strtok(NULL, " ");
-				// printf("%d\n", strcmp(path, "/"));
-				if(strcmp(requestType, "GET") == 0) { //get request
-					puts(path);
-					printf("%d\n", strlen(path));
-					char* fileContents = openFile(path, root);
-					puts(fileContents);
-					if(fileContents == NULL) {
-						createHTTPResponse(HTTPResponse, 404, "File could not be found. Maybe somewhere else?");
-					}
-					else {
-						createHTTPResponse(HTTPResponse, 200, fileContents);
-					}
-					send(client_sock, HTTPResponse, strlen(HTTPResponse), 0);
-				}
-				else if(strcmp(requestType, "POST") == 0) { //post request
-
-					char* fileQuery = strtok(path, "?");
-					char* dataQuery = strtok(NULL, " ");
-					char* fileData = replaceWord(strstr(dataQuery, "fileData=") + 9, "%20", " ");
-					char* fileName = strtok(strstr(dataQuery, "fileName=") + 9, "&");
-					printf("%s\n", fileQuery);			
-					printf("%s\n", fileData);
-					printf("%s\n", fileName);	
-					printf("%d\n", strlen(fileQuery));
-					char rootQuery[100];
-					char fileNameCopy[strlen(fileName)]; 
-					if(strlen(fileQuery) > 1) { //Are we adding to sub-directory?
-						fileNameCopy[0] = '/';
-						puts(fileQuery);
-						if(getDirectoryFromPath(fileQuery, root) == NULL) {
-							makeDirectory(fileQuery + 1, root);
-						}
-						printf("figidfgdfigid %s\n", root->childDirs->data->name);
-					}
-					strcpy(rootQuery, fileQuery);
-					strcat(fileNameCopy, fileName);
-					strcat(rootQuery, fileNameCopy);
-
-					//uploadFile("/cooltext.txt", root, "..", cooltext.txt);
-					uploadFile(fileQuery, root, fileData, fileName);
-					// puts(openFile(rootQuery, root));
-					// puts(fileData);
-					// printf("%d\n", strcmp(openFile(rootQuery, root), fileData));
-
-					char HTMLResponse[100];
-					// puts(rootQuery);
-					if(strcmp(openFile(rootQuery, root), fileData) == 0) {
-						sprintf(HTMLResponse, "File %s created successfully at path %s", fileName, rootQuery);
-						createHTTPResponse(HTTPResponse, 200, HTMLResponse);
-						printf("==================\n");
-						printf("%s\n", openFile(rootQuery, root));
-					}
-					else {
-						sprintf(HTMLResponse, "File %s not created successfully. Something went wrong.", fileName);
-						createHTTPResponse(HTTPResponse, 400, HTMLResponse);
-					}
-					printf("RESPONSE:\n%s\n", HTTPResponse);
-					send(client_sock, HTTPResponse, strlen(HTTPResponse), 0);
-				}
-				else if(strcmp(requestType, "DELETE") == 0) { //delete request
-
-				}
-				else {
-					// send(client_sock, hello, strlen(hello), 0);
-				}
-			}
-			else {
-				// write(client_sock, error_message, strlen(error_message));
-			}
-			// printf("REALPATH IS: %s\n", realpath(buffer, actualpath));
-			// // if(realpath(buffer, actualpath) == NULL) {
-			// // 	printf("ERROR: %s is an incorrect path.\n", buffer);
-			// // 	close(client_sock);
-			// // 	return 0;
-			// // }
-			// FILE *fp = fopen(actualpath, "r");
-			// if(fp == NULL) {
-			// 	printf("Error in opening file.");
-			// 	close(client_sock);
-			// 	return 0;
-			// }
-
-			// while((bytesread = fread(buffer, 1, 4096, fp)) > 0) {
-			// 	write(client_sock, buffer, bytesread);
-			// }
-			// if(msgsize == bytesread) write(client_sock, hello, strlen(hello));
-			// send(server_socket, message, strlen(message), 0);
-			
-			// fclose(fp);
+		if(pthread_create(&sniffer_thread, NULL, connection_handler, (void*) new_sock) < 0) {
+			perror("Couldn't make new thread.");
+			exit(1);
 		}
+
+		pthread_join(sniffer_thread, NULL);
+		
+		// Begin receiving HTTP requests (either from the browser, Postman, or client)
 		close(client_sock);
 		fflush(stdout);
 		puts("Waiting for incoming connections...");
@@ -326,6 +199,127 @@ int main(int argc, char *argv[])
 		// 	perror("recv failed");
 		// }
 	}
-	
 	return 0;
 }
+
+void connection_handler(void* socket_desc) {
+	char buffer[4096];
+	char actualpath[PATH_MAX + 1];
+	char HTTPResponse[4092];
+	int client_sock = *(int*)socket_desc;
+	int msgsize = 0;
+	size_t bytesread;
+	
+	while((bytesread = read(client_sock, buffer+msgsize, sizeof(buffer)-msgsize-1)) > 0) {	
+		msgsize += bytesread;
+		if(msgsize > 4095) break;
+		buffer[msgsize-1] = 0;
+		// Send the message back to client
+		if(msgsize == bytesread) printf("REQUEST IS:\n%s\n", buffer);
+		//HTTP Parsing
+		char* requestType = strtok(buffer, " "); // GET, POST, DELETE
+		char* path;
+		char fileContents[10000];
+
+
+		if(requestType != NULL) {
+			path = strtok(NULL, " ");
+			if(strcmp(requestType, "GET") == 0) { //get request
+				getRealPath(path, actualpath); //realpath gets stored to actualpath
+				getFileContents(actualpath, fileContents);
+				if(fileContents == NULL) {
+					createHTTPResponse(HTTPResponse, 404, "File could not be found. Maybe somewhere else?");
+				}
+				else {
+					createHTTPResponse(HTTPResponse, 200, fileContents);
+				}
+				send(client_sock, HTTPResponse, strlen(HTTPResponse), 0);
+			}
+			else if(strcmp(requestType, "POST") == 0) { //post request
+				char* fileQuery = strtok(path, "?");
+				char* dataQuery = strtok(NULL, " ");
+				char* fileData = replaceWord(strstr(dataQuery, "fileData=") + 9, "%20", " ");
+				char* fileName = strtok(strstr(dataQuery, "fileName=") + 9, "&");
+				printf("%s\n", fileQuery);			
+				printf("%s\n", fileData);
+				printf("%s\n", fileName);	
+				printf("%d\n", strlen(fileQuery));
+				char rootQuery[100];
+				char fileNameCopy[strlen(fileName)]; 
+				// if(strlen(fileQuery) > 1) { //Are we adding to sub-directory?
+				// 	fileNameCopy[0] = '/';
+				// 	puts(fileQuery);
+				// 	if(getDirectoryFromPath(fileQuery, root) == NULL) {
+				// 		makeDirectory(fileQuery + 1, root);
+				// 	}
+				// }
+				strcpy(rootQuery, fileQuery);
+				strcat(fileNameCopy, fileName);
+				strcat(rootQuery, fileNameCopy);
+
+				//uploadFile("/cooltext.txt", root, "..", cooltext.txt);
+				// uploadFile(fileQuery, root, fileData, fileName);
+				// puts(openFile(rootQuery, root));
+				// puts(fileData);
+				// printf("%d\n", strcmp(openFile(rootQuery, root), fileData));
+
+				char HTMLResponse[100];
+				// puts(rootQuery);
+				// if(strcmp(openFile(rootQuery, root), fileData) == 0) {
+				// 	sprintf(HTMLResponse, "File %s created successfully at path %s", fileName, rootQuery);
+				// 	createHTTPResponse(HTTPResponse, 200, HTMLResponse);
+				// 	printf("==================\n");
+				// 	printf("%s\n", openFile(rootQuery, root));
+				// }
+				// else {
+				// 	sprintf(HTMLResponse, "File %s not created successfully. Something went wrong.", fileName);
+				// 	createHTTPResponse(HTTPResponse, 400, HTMLResponse);
+				// }
+				// printf("RESPONSE:\n%s\n", HTTPResponse);
+				// send(client_sock, HTTPResponse, strlen(HTTPResponse), 0);
+			}
+			else if(strcmp(requestType, "DELETE") == 0) { //delete request
+
+			}
+			else {
+				// send(client_sock, hello, strlen(hello), 0);
+			}
+		}
+	}
+}
+
+void getRealPath(char* path, char* actualpath) {
+	char relativePath[strlen(path) + 9];
+	char* rootPointer = "../root";
+	memcpy(relativePath, rootPointer, 9);
+	strcat(relativePath, path);
+	printf("Relative path is: %s\n", relativePath);
+	if(realpath(relativePath, actualpath) == NULL) {
+		printf("ERROR: %s is an incorrect path.\n", relativePath);
+		exit(1);
+	}
+	else {
+		printf("Path is actually: %s\n", actualpath);
+	}
+} 
+
+void getFileContents(char actualpath[], char fileContents[]) {
+	// puts(actualpath);
+	FILE *fp = fopen(actualpath, "r");
+	if(fp == NULL) {
+		puts("Error in opening file.\n");
+		exit(1);
+	}
+	else {
+		int c;
+		int i = 0;
+		while ((c = getc(fp)) != EOF){
+			// putchar(c);
+			fileContents[i] = c;
+			i++;
+		}
+		fileContents[i] = "\0";
+		fclose(fp);
+	}
+}
+
